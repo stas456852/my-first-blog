8d6e78d72b1b2f4ec92191131341096aebcfef78
Stas <stasbetmen98@gmail.com>
2020-03-09 21:13:55 +0200

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     path('admin/', admin.site.urls),
+
 ]
